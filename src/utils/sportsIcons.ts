bdc0ef25136b2eed9c7c124022e6d5c59f257d56
c1c3ru <cicerosilva.ifce@gmail.com>
2025-02-25 14:33:46 -0300

    Refactor: Corrigir a exportação da constante SportsIcons e adicionar função getSportIcon

--- src/utils/sportsIcons.ts
+++ src/utils/sportsIcons.ts
@@ -3,12 +3,12 @@ import SportsSoccerIcon from "@mui/icons-material/SportsSoccer"; // Futebol
 import SportsBasketballIcon from "@mui/icons-material/SportsBasketball"; // Basquete
 import SportsVolleyballIcon from "@mui/icons-material/SportsVolleyball"; // Vôlei
 import SportsHandballIcon from "@mui/icons-material/SportsHandball"; // Handebol
-
+export const getSportIcon = (sport: SportEnum) => {
 // Mapeamento de esportes para ícones do Material-UI
-export const SportsIcons: Record<SportEnum, React.ComponentType> = {
+ const SportsIcons: Record<SportEnum, React.ComponentType> = {
   [SportEnum.FUTSAL]: SportsSoccerIcon, // Futsal usa o ícone de futebol
   [SportEnum.SOCCER]: SportsSoccerIcon, // Futebol
   [SportEnum.VOLLEYBALL]: SportsVolleyballIcon, // Vôlei
   [SportEnum.BASKETBALL]: SportsBasketballIcon, // Basquete
   [SportEnum.HANDBALL]: SportsHandballIcon, // Handebol
-};
+}};
